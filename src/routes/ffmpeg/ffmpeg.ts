5e65320f387bd69fe06367c5ecab20ff49d3ca8b
ykawaguchi <ykawaguchi.1227@gmail.com>
2021-06-28 17:17:35 +0900

    ffmpegがnodejs、typescriptで動作することを確認 fsを含めて使い方がわからないため、正しく動作していない

--- src/routes/ffmpeg/ffmpeg.ts
+++ src/routes/ffmpeg/ffmpeg.ts
@@ -1,6 +1,32 @@
 import { NextFunction, Request, Response } from "express";
+import ffmpeg from 'fluent-ffmpeg';
+import fs from 'fs'
 
-export const test = (req: Request, res: Response, next: NextFunction) => {
-  res.send(req.params.text);
+const PATH = "";
+
+export const test = async (req: Request, res: Response, next: NextFunction) => {
+  //let command = ffmpeg();
+  let stream = fs.createWriteStream(PATH);
+  let readstream = fs.createReadStream(PATH)
+  let command = await ffmpeg().input(readstream)
+  console.log(readstream)
+  /*.frames(30)
+  .size('640x480')
+  .autopad(true, 'white')
+  .output('file.wmv')
+  .output(stream);*/
+  res.send("ok");
 };
 
+const fixMedia = async (
+  input_file: string,
+  output_file: string
+) => {
+  // input_fileを読み込んでoutput_fileを生成する
+  const converted = await ffmpeg(input_file)
+    .fps(30.0)
+    .audioBitrate(192)
+    .on('end', () => {
+      console.log(`変換完了`);
+    }).save(output_file);
+}
