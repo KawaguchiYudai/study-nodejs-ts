d115032a6a0445e7b02a9f4de696ec2a833c381c
ykawaguchi <ykawaguchi.1227@gmail.com>
2021-06-30 15:26:30 +0900

    サンプル１を追加

--- src/routes/ffmpeg/ffmpeg.ts
+++ src/routes/ffmpeg/ffmpeg.ts
@@ -2,31 +2,52 @@ import { NextFunction, Request, Response } from "express";
 import ffmpeg from 'fluent-ffmpeg';
 import fs from 'fs'
 
-const PATH = "";
+const PATH = './material';
 
 export const test = async (req: Request, res: Response, next: NextFunction) => {
-  //let command = ffmpeg();
-  let stream = fs.createWriteStream(PATH);
-  let readstream = fs.createReadStream(PATH)
-  let command = await ffmpeg().input(readstream)
-  console.log(readstream)
-  /*.frames(30)
-  .size('640x480')
-  .autopad(true, 'white')
-  .output('file.wmv')
-  .output(stream);*/
+  sample1(PATH + '/base/movie/Waves.mp4', PATH + '/result/movie/sample1.mp4');
   res.send("ok");
 };
 
-const fixMedia = async (
-  input_file: string,
-  output_file: string
-) => {
-  // input_fileを読み込んでoutput_fileを生成する
-  const converted = await ffmpeg(input_file)
-    .fps(30.0)
-    .audioBitrate(192)
-    .on('end', () => {
-      console.log(`変換完了`);
-    }).save(output_file);
+//ファイル名を変更し、生成
+const sample1 = async (input_file: string, output_file: string) => {
+  let readstream = fs.createReadStream(input_file)
+  let writestream = fs.createWriteStream(output_file);
+  ffmpeg(readstream)
+    .addOutputOptions('-movflags +frag_keyframe+separate_moof+omit_tfhd_offset+empty_moov')
+    .format('mp4')
+    .on('start', () => { console.log(`変換開始`); })
+    .on('end', () => { console.log(`変換完了`); })
+    .on('error', function (err) { console.log('an error happened: ' + err.message); })
+    .pipe(writestream)
+}
+
+/*公式サイト　https://github.com/fluent-ffmpeg/node-fluent-ffmpeg
+参考サイト*/
+const ffmpegDefult = () => {
+  let filepath = "";
+  //・FFmpegコマンドの作成
+  //let command = new ffmpeg.FfmpegCommand();
+  //let command = ffmpeg();
+  //let command = ffmpeg(filepath);
+  //let command = ffmpeg(fs.createReadStream(filepath));
+  ////options
+  //source
+  //timeout
+  //preset or presets
+  //niceness or priority
+  //logger
+  //stdoutLines
+
+  //・inputs
+  ffmpeg(filepath)
+    .input(filepath)
+    .input(fs.createReadStream(filepath))
+    .addInput(filepath)
+    .mergeAdd(filepath)
+    ////inputsoption
+    .inputFormat('mov')
+  //.fromFormat()
+  //.withInputFormat()
+
 }
