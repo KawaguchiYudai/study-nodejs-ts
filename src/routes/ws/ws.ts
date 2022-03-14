9e4f739188114a85a45c0fa906d8a112270b4e8c
ykawaguchi <ykawaguchi.1227@gmail.com>
2022-03-14 17:46:01 +0900

    テストを作成、番号呼び出し動作を行う

--- src/routes/ws/ws.ts
+++ src/routes/ws/ws.ts
@@ -1,22 +1,82 @@
 import * as ws from 'ws';
 
+const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const N = 8;
+
+let standbyList: string[] = [];
+let callList: string[] = [];
+
+/*
+data={
+    action:'',
+    {
+        create
+        add
+        move-standby
+        move-call
+        delete
+    }
+    id: '',
+    standbyList: [],
+        callList:[],
+        version: ''
+}
+*/
+
 export const wsTest = () => {
-    var server = new ws.Server({ port: 5001 });
+    let server = new ws.Server({ port: 5001 });
 
     // 接続時に呼ばれる
-    server.on('connection', (ws: any) => {
+    server.on('connection', (s: any) => {
         // クライアントからのデータ受信時に呼ばれる
-        ws.on('message', (message: any) => {
-            console.log(message.toString());
+        s.on('message', (message: any) => {
 
-            // クライアントにデータを返信
+            const data = JSON.parse(message.toString());
+            switch (data.action) {
+                case 'create':
+                    const id = Array.from(Array(N)).map(() => S[Math.floor(Math.random() * S.length)]).join('');
+                    standbyList.push(id);
+                    const createdata = {
+                        action: 'create',
+                        id: id,
+                        standbyList: standbyList,
+                        callList: callList,
+                        version: (new Date().toString())
+                    }
+                    //返信//個人
+                    s.send(JSON.stringify(createdata));
+                    break;
+                case 'add':
+                    standbyList.push(data.id);
+                    break;
+                case 'move-standby':
+                    callList = callList.filter((element) => { return element !== data.id });
+                    standbyList.push(data.id);
+                    break;
+                case 'move-call':
+                    standbyList = standbyList.filter((element) => { return element !== data.id });
+                    callList.push(data.id);
+                    break;
+                case 'delete':
+                    standbyList = standbyList.filter((element) => { return element !== data.id });
+                    callList = callList.filter((element) => { return element !== data.id });
+                    break;
+            }
+
+            const senddata = {
+                action: 'update',
+                standbyList: standbyList,
+                callList: callList,
+                version: (new Date().toString())
+            }
+            // クライアントにデータを返信//全員に
             server.clients.forEach((client: any) => {
-                client.send(message.toString());
+                client.send(JSON.stringify(senddata));
             });
         });
 
         // 切断時に呼ばれる
-        ws.on('close', () => {
+        s.on('close', () => {
             console.log('close');
         });
     });
